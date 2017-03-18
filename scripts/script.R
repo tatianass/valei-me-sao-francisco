b785b15002a8044884995ab8bfe7e227dda95fe9
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2017-03-18 16:28:04 -0300

    dados da água, população, esgoto

--- scripts/script.R
+++ scripts/script.R
@@ -5,9 +5,9 @@ library(dplyr)
 library(sqldf)
 
 #lê csv's
-esgoto <- read.csv("../dados/info_esgoto.csv", header = T, sep = ",", encoding = "UTF-8", stringsAsFactors = F)
+esgoto <- read.csv("../dados/indicadores_operacionais_esgoto.csv", header = T, sep = ",", encoding = "UTF-8", stringsAsFactors = F)
 municipios <- read.csv("../dados/municipios_favorecidos_sao_francisco.csv", header = T, sep = ";", encoding = "UTF-8", stringsAsFactors = F)
 municipio_esgoto <- left_join(municipios, esgoto, by = c("MUNICIPIO" = "municipio"))
-municipio_esgoto <- sqldf("SELECT * FROM municipios INNER JOIN esgoto ON municipios.MUNICIPIO = esgoto.municipio ")
+municipio_esgoto <- sqldf("SELECT * FROM esgoto WHERE Ano.de.Referência = 2015 ")
 
 esgoto <- filter("Ano.de.Referência" == 2015)
