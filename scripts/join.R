fbccdb9043c7159e52d83d958d16c03de5b16885
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2017-03-19 14:38:22 -0300

    resolvendo problema no lapply

--- scripts/join.R
+++ scripts/join.R
@@ -9,23 +9,24 @@ beneficiados <- read.csv("../arcgis/municipios_beneficiados.csv", header = T, en
 join <- left_join(beneficiados, mun, by = c("nome" = "municipio"))
 
 #selecting desired columns
-join <- join %>% select(FID, nome, uf.x, codigo_ibg, codigo_mun, populacao.x, agua.x, esgoto.x, ano.y)
+join <- join %>% select(FID, nome, populacao.x, agua.x, esgoto.x, ano.y)
 
 #renaming columns
-names <- c("id", "nome", "uf", "codigo_ibg", "codigo_mun", "populacao", "agua", "esgoto", "ano")
+names <- c("id", "nome", "populacao", "agua", "esgoto", "ano")
 colnames(join) <- names
 
 #creating percentage
 m_hist <- mutate(join, taxa_esgoto = esgoto/populacao, taxa_agua = agua/populacao)
 
-write.table(join, file = "municipios_hist.csv", sep = ",", col.names = T, quote = T, fileEncoding = "UTF-8")
+#write.table(join, file = "municipios_hist.csv", sep = ",", col.names = T, quote = T, fileEncoding = "UTF-8")
 
 # transform the data.frame into the described structure
-idsIndexes <- which(names(m_hist) != 'id' & names(m_hist) != 'nome')
+#idsIndexes <- which(names(m_hist) != 'id' & names(m_hist) != 'nome')
+idsIndexes <- c(1)
 a <- lapply(1:nrow(m_hist),FUN=function(i){ 
   list(municipio=list(id=m_hist[i,'id'], nome=m_hist[i, 'nome']),
        valores=lapply(idsIndexes,
-                      FUN=function(j)list(ano=m_hist[i,'ano'], agua=m_hist[i,'agua'], esgoto=m_hist[i,'esgoto'], populacao=m_hist[i,'populacao'], taxa_agua=m_hist[i,'taxa_agua'], taxa_esgoto=m_hist[i,'taxa_esgoto'])))
+                      FUN=function(j)list(populacao=m_hist[i,'populacao'], agua=m_hist[i,'m_hist'], esgoto=m_hist[i,'esgoto'],  ano=m_hist[i,'ano'], taxa_esgoto=m_hist[i,'taxa_esgoto'], taxa_agua=m_hist[i,'taxa_agua'])))
 })
 
 # serialize to json
