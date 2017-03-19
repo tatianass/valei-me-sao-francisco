48cc4fa3da5d64e3c55d19cd7f7b5bf62330c675
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2017-03-19 10:26:49 -0300

    salvando com utf-8 encode

--- scripts/script.R
+++ scripts/script.R
@@ -1,4 +1,5 @@
 library(dplyr)
 municipios_beneficiados <- read.csv("../arcgis/populacao_agua_esgoto.csv", header = T, encoding = "UTF-8", stringsAsFactors = F, sep = ";")
 dados <- mutate(municipios_beneficiados, taxa_esgoto = esgoto/populacao, taxa_agua = agua/populacao)
-write.table(dados, file = "taxas.csv", sep = ",", col.names = T, quote = T)
+write.table(dados, file = "taxas.csv", sep = ",", col.names = T, quote = T, fileEncoding = "UTF-8")
+taxas <- read.csv("taxas.csv", header = T, encoding = "UTF-8", stringsAsFactors = F)
