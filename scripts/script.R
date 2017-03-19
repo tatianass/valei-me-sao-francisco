16477ab2afc97f232bc4128db99dc85ba9d01cab
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2017-03-19 10:16:07 -0300

    adicionando taxa ao csv

--- scripts/script.R
+++ scripts/script.R
@@ -1,4 +1,4 @@
 library(dplyr)
-dados <- read.csv("../arcgis/municipios_beneficiados.csv", header = T, encoding = "UTF-8", stringsAsFactors = F, sep = ",")
-dados <- mutate(dados, taxa_esgoto = esgoto/populacao, taxa_agua = agua/populacao)
-write.table(dados, file = "taxas.csv", sep = ",", col.names = T, fileEncoding = "UTF-8", quote = F)
+municipios_beneficiados <- read.csv("../arcgis/populacao_agua_esgoto.csv", header = T, encoding = "UTF-8", stringsAsFactors = F, sep = ";")
+dados <- mutate(municipios_beneficiados, taxa_esgoto = esgoto/populacao, taxa_agua = agua/populacao)
+write.table(dados, file = "taxas.csv", sep = ",", col.names = T, quote = T)
