5e55a9c78d58f3791a59d4e201a38e01827910b3
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2017-03-18 22:36:08 -0300

    dados corrigidos nome: populacao_agua_esgoto.csv

--- scripts/script.R
+++ scripts/script.R
@@ -1,13 +1,9 @@
-#install.packages("dplyr", dependencies = T)
-#install.packages("assertthat", dependencies = T)
-install.packages("sqldf", dependencies = T)
-library(dplyr)
-library(sqldf)
+install.packages("car")
 
-#lê csv's
-esgoto <- read.csv("../dados/indicadores_operacionais_esgoto.csv", header = T, sep = ",", encoding = "UTF-8", stringsAsFactors = F)
-municipios <- read.csv("../dados/municipios_favorecidos_sao_francisco.csv", header = T, sep = ";", encoding = "UTF-8", stringsAsFactors = F)
-municipio_esgoto <- left_join(municipios, esgoto, by = c("MUNICIPIO" = "municipio"))
-municipio_esgoto <- sqldf("SELECT * FROM esgoto WHERE Ano.de.Referência = 2015 ")
+library(car)
+dados <- read.csv("../arcgis/populacao_agua_esgoto.csv", header = T, encoding = "UTF-8", stringsAsFactors = F, sep = ";")
 
-esgoto <- filter("Ano.de.Referência" == 2015)
+scatterplot(mpg ~ wt | cyl, data=mtcars, 
+            xlab="Weight of Car", ylab="Miles Per Gallon", 
+            main="Enhanced Scatter Plot", 
+            labels=row.names(mtcars))
