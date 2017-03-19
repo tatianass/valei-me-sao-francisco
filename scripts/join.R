073718461ec9c4da4e6e2ce336937f4f354cc399
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2017-03-19 14:53:48 -0300

    concertando script do join

--- scripts/join.R
+++ scripts/join.R
@@ -9,7 +9,7 @@ beneficiados <- read.csv("../arcgis/municipios_beneficiados.csv", header = T, en
 join <- left_join(beneficiados, mun, by = c("nome" = "municipio"))
 
 #selecting desired columns
-join <- join %>% select(FID, nome, populacao.x, agua.x, esgoto.x, ano.y)
+join <- join %>% select(FID, nome, populacao.y, agua.y, esgoto.y, ano.y)
 
 #renaming columns
 names <- c("id", "nome", "populacao", "agua", "esgoto", "ano")
@@ -18,11 +18,10 @@ colnames(join) <- names
 #creating percentage
 m_hist <- mutate(join, taxa_esgoto = esgoto/populacao, taxa_agua = agua/populacao)
 
-#write.table(join, file = "municipios_hist.csv", sep = ",", col.names = T, quote = T, fileEncoding = "UTF-8")
+write.table(m_hist, file = "municipios_hist.csv", sep = ",", col.names = T, quote = T, fileEncoding = "UTF-8")
 
 # transform the data.frame into the described structure
-#idsIndexes <- which(names(m_hist) != 'id' & names(m_hist) != 'nome')
-idsIndexes <- c(1)
+idsIndexes <- which(names(m_hist) != 'id' & names(m_hist) != 'nome')
 a <- lapply(1:nrow(m_hist),FUN=function(i){ 
   list(municipio=list(id=m_hist[i,'id'], nome=m_hist[i, 'nome']),
        valores=lapply(idsIndexes,
