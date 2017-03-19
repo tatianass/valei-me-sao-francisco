118bb0af7257a24a3d5def923b7d8a84929a5215
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2017-03-19 12:48:57 -0300

    criando json com o histórico dos municípios

--- README.md
+++ README.md
@@ -10,3 +10,25 @@ Run `grunt` for building and `grunt serve` for preview.
 ## Testing
 
 Running `grunt test` will run the unit tests with karma.
+
+
+## Generating data
+The next steps were made in ArcGIS and R:
+
+# From ArcGIS
+1. Open 'valei_me.mxd' in ArcGIS
+2. Right click on the map;
+3. Choose 'Open Attribute Table';
+4. Click on 'Table Options' (left side on top);
+5. Choose 'Export'
+6. Click on 'Browse' button
+7. In 'Save as type' choose 'Text File'
+8. Save with <your file name>.csv.
+
+PS: 
+- Most of the data was obtained using join between the shape and others csv files. See (1) Right click on the map; (2) 'Join and Relate'; (3) 'Join...'
+- You can also delete any field that you are not going to use before export: Right click on the table column and 'Delete Field'
+- When loading your csv in ArcGIS, don't forgot to check the data types: Right click on the table column and 'Properties'.
+
+# From R
+Just use the 'script.R' in scripts folder.
--- scripts/script.R
+++ scripts/script.R
@@ -1,5 +1,34 @@
 library(dplyr)
-municipios_beneficiados <- read.csv("../arcgis/populacao_agua_esgoto.csv", header = T, encoding = "UTF-8", stringsAsFactors = F, sep = ";")
-dados <- mutate(municipios_beneficiados, taxa_esgoto = esgoto/populacao, taxa_agua = agua/populacao)
-write.table(dados, file = "taxas.csv", sep = ",", col.names = T, quote = T, fileEncoding = "UTF-8")
+library(?jsonlite) 
+
+#loading data
+municipios_esgoto_agua <- read.csv("../arcgis/populacao_agua_esgoto.csv", header = T, encoding = "UTF-8", stringsAsFactors = F, sep = ";")
+municipios_esgoto_agua_hist <- read.csv("../arcgis/populacao_agua_esgoto_hist.csv", header = T, encoding = "UTF-8", stringsAsFactors = F, sep = ";")
+
+#creating percentages
+m <- mutate(municipios_esgoto_agua, taxa_esgoto = esgoto/populacao, taxa_agua = agua/populacao)
+m_hist <- mutate(municipios_esgoto_agua_hist, taxa_esgoto = esgoto/populacao, taxa_agua = agua/populacao)
+
+#selecting variables to create json file
+m_hist <- m_hist %>% select(id, nome, ano, agua, esgoto, populacao, taxa_esgoto, taxa_agua)
+
+# transform the data.frame into the described structure
+idsIndexes <- which(names(m_hist) != 'id' & names(m_hist) != 'nome')
+a <- lapply(1:nrow(m_hist),FUN=function(i){ 
+  list(municipio=list(id=m_hist[i,'id'], nome=m_hist[i, 'nome']),
+       valores=lapply(idsIndexes,
+                      FUN=function(j)list(ano=m_hist[i,'ano'], agua=m_hist[i,'agua'], esgoto=m_hist[i,'esgoto'], populacao=m_hist[i,'populacao'], taxa_agua=m_hist[i,'taxa_agua'], taxa_esgoto=m_hist[i,'taxa_esgoto'])))
+})
+
+# serialize to json
+txt <- toJSON(a)
+# if you want, indent the json
+txt <- prettify(txt)
+write(txt, "municipios_hist.json")
+
+#saving data
+write.table(m, file = "taxas.csv", sep = ",", col.names = T, quote = T, fileEncoding = "UTF-8")
+
+#checking data saved
 taxas <- read.csv("taxas.csv", header = T, encoding = "UTF-8", stringsAsFactors = F)
+
